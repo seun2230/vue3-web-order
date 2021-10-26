f654286be04d1f5755a23f2501d27ea218c6c730
강호진 <battlemech@naver.com>
2021-10-26 14:08:08 +0900

    eslint 문제 일단 해결. 페이지 로드 성공

---
+++ web-order/src/components/SignupForm.vue
@@ -0,0 +1,241 @@
+<template>
+    <div class="container">
+        <h2 class="title">회원 가입</h2>
+        <form class="form" method="post" action="#" v-on:submit.prevent="submitForm">
+
+            <div class="input-group">
+                <label for="username" class="label">이름</label>
+                <input
+                    type="text"
+                    id="username"
+                    class="input"
+                    placeholder="이름을 입력하세요."
+                    v-model="username">
+                <span class="error-message"></span>
+                <svg class="icon icon-success hidden hidden" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clip-rule="evenodd" />
+                </svg>
+
+                <svg class="icon icon-error hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+
+            <div class="input-group">
+                <label for="email" class="label">이메일</label>
+                <input
+                    type="email"
+                    id="email"
+                    class="input"
+                    placeholder="이메일을 입력하세요."
+                    v-model = "email">
+                <span class="error-message"></span>
+                <svg class="icon icon-success hidden" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clip-rule="evenodd" />
+                </svg>
+
+                <svg class="icon icon-error hidden" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+
+            <div class="input-group">
+                <label for="password" class="label">비밀번호</label>
+                <input
+                    type="password"
+                    id="password"
+                    class="input"
+                    v-model="password">
+                <span class="error-message"></span>
+                <svg class="icon icon-success hidden" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clip-rule="evenodd" />
+                </svg>
+
+                <svg class="icon icon-error hidden" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+
+            <div class="input-group">
+                <label for="password_confirmation" class="label">비밀번호 확인</label>
+                <input
+                    type="password"
+                    id="password_confirmation"
+                    class="input"
+                    v-model="passwordConfirmation">
+                <span class="error-message"></span>
+                <svg class="icon icon-success hidden" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clip-rule="evenodd" />
+                </svg>
+
+                <svg class="icon icon-error hidden" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+
+            <input type="submit" class="button" value="회원 가입">
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignupForm',
+    data () {
+        return {
+            username: '',
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+        };
+    },
+    methods: {
+        submitForm() {
+            this.$router.push('/');
+        }
+    }
+}
+</script>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+    /* 박스의 크기 계산 기준 */
+}
+
+body {
+    background-color:burlywood;
+}
+
+.title {
+    margin-bottom: 2rem;
+}
+
+.hidden {
+    display: none;
+}
+
+.icon {
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    top: 32px;
+    right: 5px;
+    pointer-events: none;
+    /* 해당 영역의 이벤트에 응답하지 않도록 */
+    z-index: 2;
+}
+
+.icon.icon-success {
+    fill: green;
+}
+
+.icon-error {
+    fill: red;
+}
+
+.container {
+    max-width: 460px;
+    margin: 3rem auto;
+    padding: 3rem;
+    border: 1px solid #ddd;
+    border-radius: .25rem;
+    background-color: white;
+    box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+        0 10px 10px -5px rgba(0, 0, 0, 0.04);
+}
+
+.label {
+    text-align: left;
+    font-weight: bold;
+    display: block;
+    color: #333;
+    margin-bottom: .25rem;
+    color: #2d3748;
+}
+
+.input {
+    appearance: none;
+    /* 운영체제 및 브라우저에 기본 설정된 테마를 기반으로 요소 표현
+        : 네이티브로 지원되는 모양들을 해제 또는 추가
+        -> ios, 안드로이드 간의 모양 차이 없애기 -- 모든 브라우저 완벽지원은 안 함 */
+    display: block;
+    width: 100%;
+    color: #2d3748;
+    border: 1px solid #cdb5e0;
+    line-height: 1.25;
+    background-color: white;
+    padding: .65rem .75rem;
+    border-radius: 0.25rem;
+    box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.06);
+}
+
+.input::placeholder {
+    /* :: -> pseudo-element(의사 요소) 선택자 */
+    color: #a0aec0;
+}
+
+.input.input-error {
+    border: 1px solid red;
+}
+
+.input.input-error:focus {
+    border: 1px solid red;
+}
+
+.input:focus {
+    outline: none;
+    border: 1px solid #a0aec0;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
+        0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    background-clip: padding-box;
+    /* 요소의 배경이 테두리, 안쪽 여백, 콘텐츠 상자 중 어디까지 차지할 지 지정 */
+}
+
+.input-group {
+    margin-bottom: 2rem;
+    position: relative;
+}
+
+.error-message {
+    font-size: .85rem;
+    color: red;
+}
+
+.button {
+    background-color: royalblue;
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: .25rem;
+    color: white;
+    font-weight: bold;
+    display: block;
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+}
+
+.button:hover {
+    /* : 와 :: 를 구분할 것
+        -> : 가상 클래스 - 특정 부분에 스타일
+        -> :: 가상 요소 - 특정 상태에 스타일 */
+        filter: brightness(110%);
+}
+</style>
--- web-order/src/components/SignupForm2.js
+++ web-order/src/components/SignupForm2.js
@@ -1,132 +1,132 @@
-class FormValidator {
-    constructor(form, fields) {
-        this.form = form;
-        this.fields = fields;
-    }
+// class FormValidator {
+//     constructor(form, fields) {
+//         this.form = form;
+//         this.fields = fields;
+//     }
 
-    initialize() {
-        this.validateOnEntry();
-        this.validateOnSubmit();
-    }
+//     initialize() {
+//         this.validateOnEntry();
+//         this.validateOnSubmit();
+//     }
 
-    validateOnSubmit() {
-        let self = this;
+//     validateOnSubmit() {
+//         let self = this;
         
-        self.form.addEventListener('submit', e => {
-            e.preventDefault();     //  html에서의 a 또는 submit의 페이지 이동, 전송 등의 동작 중단
-            self.fields.forEach(field => {  //  forEch -> 입력값 각 문자에 대해 검증시도
-                const input = document.querySelector(`#${field}`);
-                self.validateFields(input)
-            });
-            form.submit();
+//         self.form.addEventListener('submit', e => {
+//             e.preventDefault();     //  html에서의 a 또는 submit의 페이지 이동, 전송 등의 동작 중단
+//             self.fields.forEach(field => {  //  forEch -> 입력값 각 문자에 대해 검증시도
+//                 const input = document.querySelector(`#${field}`);
+//                 self.validateFields(input)
+//             });
+//             form.submit();
             
-            // self.clearForm();
-        });
-    }
+//             // self.clearForm();
+//         });
+//     }
 
-    //  폼 삭제
-    //  이거 form.reset(); 과 같은 기능 아닌가?
-    // clearForm() {
-    //     let messageErr = [];
-    //     const errorMessages = document.querySelectorAll('.error-message');
-    //     errorMessages.forEach(err => {
-    //         if (err.innerText != "") {
-    //             messageErr.push(err.innerText);
-    //         }
-    //     });
+//     //  폼 삭제
+//     //  이거 form.reset(); 과 같은 기능 아닌가?
+//     // clearForm() {
+//     //     let messageErr = [];
+//     //     const errorMessages = document.querySelectorAll('.error-message');
+//     //     errorMessages.forEach(err => {
+//     //         if (err.innerText != "") {
+//     //             messageErr.push(err.innerText);
+//     //         }
+//     //     });
 
-    //     if (messageErr.length == 0) {
-    //         const inputs = document.querySelectorAll('input');
-    //         const svgIcons = document.querySelectorAll('.icon');
-    //         svgIcons.forEach(icon => {
-    //             icon.classList.add('hidden');
-    //         });
-    //         inputs.forEach(input => {
-    //             if (input.value != '회원 가입') {
-    //                 input.value = "";
-    //             }
-    //         });
-    //     }
-    // }
+//     //     if (messageErr.length == 0) {
+//     //         const inputs = document.querySelectorAll('input');
+//     //         const svgIcons = document.querySelectorAll('.icon');
+//     //         svgIcons.forEach(icon => {
+//     //             icon.classList.add('hidden');
+//     //         });
+//     //         inputs.forEach(input => {
+//     //             if (input.value != '회원 가입') {
+//     //                 input.value = "";
+//     //             }
+//     //         });
+//     //     }
+//     // }
 
-    validateOnEntry() {
-        let self = this;
+//     validateOnEntry() {
+//         let self = this;
 
-        this.fields.forEach(field => {
-            const input = document.querySelector(`#${field}`);
-            input.addEventListener('input', event => {
-                self.validateFields(input)
-            })
-        })
-    }
+//         this.fields.forEach(field => {
+//             const input = document.querySelector(`#${field}`);
+//             input.addEventListener('input', event => {
+//                 self.validateFields(input)
+//             })
+//         })
+//     }
 
-    validateFields(field) {
+//     validateFields(field) {
 
-        //  값 존재 여부 확인
-        if (field.value.trim() === "") {
-            if (field.id === "username") { 
-                this.setStatus(field, `${field.previousElementSibling.innerText}을 입력하세요.`, "error");
-            } else {
-                this.setStatus(field, `${field.previousElementSibling.innerText}를 입력하세요.`, "error");
-            }
-        } else {
-            this.setStatus(field, null, "success");
-        }
+//         //  값 존재 여부 확인
+//         if (field.value.trim() === "") {
+//             if (field.id === "username") { 
+//                 this.setStatus(field, `${field.previousElementSibling.innerText}을 입력하세요.`, "error");
+//             } else {
+//                 this.setStatus(field, `${field.previousElementSibling.innerText}를 입력하세요.`, "error");
+//             }
+//         } else {
+//             this.setStatus(field, null, "success");
+//         }
 
-        //  이메일 주소 유효성 검사
-        if (field.type === "email") {   
-            const re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-            if (re.test(field.value)) {
-                this.setStatus(field, null, "success");
-            } else {
-                this.setStatus(field, "유효한 이메일 주소를 입력하세요.", "error");
-            }
-        }
+//         //  이메일 주소 유효성 검사
+//         if (field.type === "email") {   
+//             const re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+//             if (re.test(field.value)) {
+//                 this.setStatus(field, null, "success");
+//             } else {
+//                 this.setStatus(field, "유효한 이메일 주소를 입력하세요.", "error");
+//             }
+//         }
 
-        //  비밀번호 확인 edge case - 경계 조건
-        if (field.id === "password_confirmation") {
-            const passwordField = this.form.querySelector('#password');
+//         //  비밀번호 확인 edge case - 경계 조건
+//         if (field.id === "password_confirmation") {
+//             const passwordField = this.form.querySelector('#password');
 
-            if (field.value.trim() == "") {
-                this.setStatus(field, "비밀번호를 한 번 더 입력하세요.", "error");
-            } else if (field.value != passwordField.value) {
-                this.setStatus(field, "비밀번호가 일치하지 않습니다.", "error");
-            } else {
-                this.setStatus(field, null, "success");
-            }
-        }   
-    }
+//             if (field.value.trim() == "") {
+//                 this.setStatus(field, "비밀번호를 한 번 더 입력하세요.", "error");
+//             } else if (field.value != passwordField.value) {
+//                 this.setStatus(field, "비밀번호가 일치하지 않습니다.", "error");
+//             } else {
+//                 this.setStatus(field, null, "success");
+//             }
+//         }   
+//     }
 
-    setStatus(field, message, status) {
-        const successIcon = field.parentElement.querySelector('.icon-success');
-        const errorIcon = field.parentElement.querySelector('.icon-error');
-        const errorMessage = field.parentElement.querySelector('.error-message');
+//     setStatus(field, message, status) {
+//         const successIcon = field.parentElement.querySelector('.icon-success');
+//         const errorIcon = field.parentElement.querySelector('.icon-error');
+//         const errorMessage = field.parentElement.querySelector('.error-message');
 
-        if (status === "success") {
-            if (errorIcon) {
-                errorIcon.classList.add('hidden');
-            }
-            if (errorMessage) {
-                errorMessage.innerText = "";
-            }
-            successIcon.classList.remove('hidden');
-            field.classList.remove('input-error');
-        }
+//         if (status === "success") {
+//             if (errorIcon) {
+//                 errorIcon.classList.add('hidden');
+//             }
+//             if (errorMessage) {
+//                 errorMessage.innerText = "";
+//             }
+//             successIcon.classList.remove('hidden');
+//             field.classList.remove('input-error');
+//         }
         
-        if (status === "error") {
-            if (successIcon) {
-                successIcon.classList.add('hidden');
-            }
-            field.parentElement.querySelector('.error-message').innerText = message;
-            errorIcon.classList.remove('hidden');
-            field.classList.add('input-error');
-        }
-    }
-}
+//         if (status === "error") {
+//             if (successIcon) {
+//                 successIcon.classList.add('hidden');
+//             }
+//             field.parentElement.querySelector('.error-message').innerText = message;
+//             errorIcon.classList.remove('hidden');
+//             field.classList.add('input-error');
+//         }
+//     }
+// }
 
-const form = document.querySelector('.form');
-const fields = ["username", "email", "password", "password_confirmation"];
+// const form = document.querySelector('.form');
+// const fields = ["username", "email", "password", "password_confirmation"];
 
-const validator = new FormValidator(form, fields)
+// const validator = new FormValidator(form, fields)
 
-validator.initialize();
+// validator.initialize();
