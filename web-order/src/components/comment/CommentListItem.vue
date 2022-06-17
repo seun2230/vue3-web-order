d575cc6c4e9ec259bbc14eab8960142004284c83
seun2230 <seun2230@gmail.com>
2022-06-17 22:13:27 +0900

    :bug: fix: Modify Common Like Button bug
    특정 게시물 좋아요 수 버튼 체크를 수정하였습니다.

--- web-order/src/components/comment/CommentListItem.vue
+++ web-order/src/components/comment/CommentListItem.vue
@@ -1,44 +1,47 @@
 <template>
-  <router-link 
-    class="container--exterrior"
-    :to="{name: 'boardItem', params: { id: comment.comments_id }}">
-    <div class="cards">
-      <!-- <img
-        :src="comment.comments_image"
-        :alt="comment.comments_id" /> -->
-      <div class="cards__image">
-        <el-avatar>
-          {{ comment.comments_user_id }}
-        </el-avatar>
-      </div>
-      <div class="cards__content">
-        <div class="cards__user">
-          <span class="cards__info">
-            {{ translateId }}
-          </span>
-          <span 
-            class="cards__date">
-            {{ comment.comments_date }}
-          </span>
-          <span>
-            주문 내역: {{ comment.food_items_food_id }}
+  <div>
+    <router-link 
+      class="container--exterrior"
+      :to="{name: 'boardItem', params: { id: comment.comments_id }}">
+      <div class="cards">
+        <!-- <img
+          :src="comment.comments_image"
+          :alt="comment.comments_id" /> -->
+        <div class="cards__image">
+          <el-avatar>
+            {{ comment.comments_id }}
+          </el-avatar>
+        </div>
+        <div class="cards__content">
+          <div class="cards__user">
+            <span class="cards__info">
+              {{ translateId }}
+            </span>
+            <span 
+              class="cards__date">
+              {{ comment.comments_date }}
+            </span>
+            <span>
+              주문 내역: {{ comment.food_items_food_id }}
+            </span>
+          </div>
+          <span class="cards__text">
+            {{ comment.comments_text }}
           </span>
         </div>
-        <span class="cards__text">
-          {{ comment.comments_text }}
-        </span>
-        <button class="cards__btn">
-          <i class="far fa-thumbs-up"></i>
-          도움돼요
-        </button>
       </div>
-    </div>
-    <!-- <CommentDetailButton /> -->
-  </router-link>
+    </router-link>
+    <button 
+      class="cards__btn"
+      @click="likeBtn(this.comment.comments_id)">
+      <i class="far fa-thumbs-up"></i>
+      도움돼요 
+    </button>
+  </div>
 </template>
 
 <script>
-// import CommentDetailButton from './CommentDetailButton.vue';
+import axios from 'axios';
 export default {
   components: {
     // CommentDetailButton
@@ -50,7 +53,14 @@ export default {
       required: true,
     },
   },
-  computed: {
+   created() {
+  console.log("created!");
+  // const id = this.$route.params.id;
+  const id = this.comment.comments_id;
+  // console.log("id check", id);
+  this.$store.commit('user/getLikeUserList', id);
+  },
+  computed: { 
     formmatDate() {
       const nowDate = this.comment.comments_date;
       const year = +nowDate.split('-')[0];
@@ -67,6 +77,18 @@ export default {
         return userId.replace(/(?<=.).(?=.)/g, "*");
       }
       return null // return 값이 없으면 안됨
+    },
+  },
+  methods: {
+    likeBtn(id) {
+      console.log("id check", id);
+      axios.post(`${process.env.VUE_APP_URL}/api/user/post/likeUp/` + id)
+      .then(res => {
+        console.log("server response", res.data);
+      })
+      .catch(err => {
+        console.log("err", err);
+      })
     },
   }
 }
