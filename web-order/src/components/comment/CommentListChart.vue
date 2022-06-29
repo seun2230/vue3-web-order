7fa4d3bf9bfb5eac25f0e913c1d8c541cf9b97c5
seun2230 <seun2230@gmail.com>
2022-06-29 15:32:37 +0900

    :sparkles: feat: Add CommentList getRatingPercent
    리뷰 게시물 페이지에 각 평점 백분율 기능을 추가했습니다.

--- web-order/src/components/comment/CommentListChart.vue
+++ web-order/src/components/comment/CommentListChart.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
-    <div>
+    <div class="progressbar-inner">
       <span>고객 총 평점</span>
-      <span>{{ getRating }}</span>
+      <span>{{ getTotalRating }}</span>
     </div>
-    <div>
-      <progress 
+    <div 
+      v-for="item in getRatingPercent"
+      :key="item">
+      <progress
         max="100"
-        value="70"
-        v-for="comment in comments"
-        :key="comment.ratings"></progress>
+        :value="item">
+      </progress>
+      <label>{{ item + '%' }}</label>
     </div>
   </div>  
 </template>
@@ -18,16 +20,37 @@
 import { mapState } from 'vuex';
 
 export default {
+  data() {
+    return {
+      name: ['1', '2', '3', '4', '5']
+    }
+  },
   computed: {
     ...mapState('comment', ['comments']),
-    getRating() {
-      const comments = this.comments;
-      const index = comments.map(comment => (comment.ratings));
-      // return index;
-      console.log("idnex",index);
-      const plusRating = index.reduce((total, val) => total + val, 0); 
+    
+    // 고객 총 평점 
+    getTotalRating() {
+      let index = this.comments.map(comment => comment.ratings);
+      let plusRating = index.reduce((total, val) => total + val, 0); 
       const Rating = (plusRating / this.comments.length).toFixed(1);  
       return Rating;
+    },
+
+    // 각각의 rating의 백분율 구하기  
+    getRatingPercent() {
+      const comments = this.comments;
+      let calcRating = this.comments.map(function(item) {
+        let ratings = item.ratings;
+        // console.log("data", rating);
+        let filterRating = comments.filter(comment => comment.ratings === ratings).length;
+        console.log("filterRating", filterRating);  // 3
+        let resultRating = ((filterRating / comments.length) * 100).toFixed(0);
+        console.log("a", resultRating);
+
+        return resultRating;
+      })
+      console.log(calcRating);
+      return calcRating;
     },
   },  
 }
@@ -38,10 +61,11 @@ export default {
   display: flex;
   flex-direction: column;
   max-width: 500px;
-  height: 150px;
+  height: 200px;
   margin: auto;
   padding: 1rem 1rem;
-  background-color: #ccc;
+  background-color: #fff;
   border-radius: .2rem;
+  box-shadow: 2px 1px 4px rgba(0, 0, 0, 0.26);
 }
 </style>
