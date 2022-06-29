d148ba668e9b27d914ec7c91bac2c8edda2ea408
seun2230 <seun2230@gmail.com>
2022-06-29 23:31:24 +0900

    :lipstick: design: Add progress design, fix getRatingPercent
    progressbar 디자인과 데이터 수치를 수정했습니다.

--- web-order/src/components/comment/CommentListChart.vue
+++ web-order/src/components/comment/CommentListChart.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="container">
-    <div class="progressbar-inner">
-      <span>고객 총 평점</span>
+    <div class="progress-inner">
       <span>{{ getTotalRating }}</span>
-    </div>
-    <div 
-      v-for="item in getRatingPercent"
-      :key="item">
-      <progress
-        max="100"
-        :value="item">
-      </progress>
-      <label>{{ item + '%' }}</label>
+      <div class="progress-content">
+        <div 
+          class="progress-item"
+          v-for="(item, index) in getRatingPercent"
+          :key="item">
+          {{ index }} 점 
+          <progress
+            max="100"
+            :value="item">
+          </progress>
+          <label :for="item">
+            {{ (( item / comments.length ) * 100).toFixed(0)+ '%' }}
+          </label>
+        </div>
+      </div>
     </div>
   </div>  
 </template>
@@ -20,11 +25,6 @@
 import { mapState } from 'vuex';
 
 export default {
-  data() {
-    return {
-      name: ['1', '2', '3', '4', '5']
-    }
-  },
   computed: {
     ...mapState('comment', ['comments']),
     
@@ -35,31 +35,30 @@ export default {
       const Rating = (plusRating / this.comments.length).toFixed(1);  
       return Rating;
     },
-
-    // 각각의 rating의 백분율 구하기  
     getRatingPercent() {
       const comments = this.comments;
-      let calcRating = this.comments.map(function(item) {
-        let ratings = item.ratings;
-        // console.log("data", rating);
-        let filterRating = comments.filter(comment => comment.ratings === ratings).length;
-        console.log("filterRating", filterRating);  // 3
-        let resultRating = ((filterRating / comments.length) * 100).toFixed(0);
-        console.log("a", resultRating);
-
-        return resultRating;
-      })
-      console.log(calcRating);
-      return calcRating;
+      let calcRating = comments.map(function(item) {
+        if(item.ratings !== null) {
+          let rating = item.ratings;
+          return rating;
+        }
+      }).filter(ratings => ratings);   // undefind 제거 완료 : [3, 5, 3, 4, 3]
+    
+      const rating = calcRating.reduce((prev, curr) => {
+        // console.log(prev[curr] ? prev[curr] + 1 : prev); // 중복값 3만 찾아옴
+        prev[curr] = (prev[curr] || 0) + 1;
+        return prev;
+      }, {});
+      return rating;
     },
   },  
 }
 </script>
 
 <style lang="scss" scoped>
+@import '@/scss/btn.scss';
+@import '@/scss/common.scss';
 .container {
-  display: flex;
-  flex-direction: column;
   max-width: 500px;
   height: 200px;
   margin: auto;
@@ -67,5 +66,31 @@ export default {
   background-color: #fff;
   border-radius: .2rem;
   box-shadow: 2px 1px 4px rgba(0, 0, 0, 0.26);
+
+  .progress-inner {
+    // background-color: purple;
+    padding-top: 10px;
+    min-width: 150px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-content: flex-end;
+    span {
+      padding: 30px;
+      display: block;
+      font-size: 18px;
+      font-weight: bold;
+      align-items: center;
+    }
+  }
 }
+
+label {
+  appearance: none;
+  font-size: 14px;
+  font-weight: none;
+  color: #ccc;
+  padding: 8px;
+}
+
 </style>
