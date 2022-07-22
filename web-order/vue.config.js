34c3cd997d2e67ed1e25897e6e65bb4f3884f62e
seun2230 <seun2230@gmail.com>
2022-07-22 12:24:03 +0900

    :construction_worker: chore: Update menu, review feature build
    메뉴, 리뷰 기능의 수정으로 빌드를 했습니다.

--- web-order/vue.config.js
+++ web-order/vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       data: `
-  //         @import '../assets/scss/_components.scss';
-  //         @import '../assets/scss/_main.scss';
-  //         @import '../assets/scss/_mixin.scss';
-  //         @import '../assets/scss/_variables.scss';
-  //         `
-  //     }
-  //   }
-  // },
   devServer: {
     proxy: {
       '/api': {
